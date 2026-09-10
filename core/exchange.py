00f98428f2b08617799382f89c52e57546a45915
wlcskf121 <415514916@qq.com>
2026-09-10 16:00:43 +0800

    Update PERP_MAP to use hyphens instead of underscores

--- core/exchange.py
+++ core/exchange.py
@@ -30,7 +30,7 @@ class ExchangeError(Exception):
     pass
 
 
-PERP_MAP = {"BTC_USDT": "BTC_USDT_SWAP", "ETH_USDT": "ETH_USDT_SWAP", "SOL_USDT": "SOL_USDT_SWAP"}
+PERP_MAP = {"BTC_USDT": "BTC-USDT-SWAP", "ETH_USDT": "ETH-USDT-SWAP", "SOL_USDT": "SOL-USDT-SWAP"}
 def _request_candlestick(base_url, instrument_name, timeframe, count=None, end_ts=None):
     instrument_name = PERP_MAP.get(instrument_name, instrument_name)
   
